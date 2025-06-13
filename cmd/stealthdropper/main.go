3d2e6efc1af59d0f773301b702a076ce80887e92
SOURYA DUTTA <110114428+Cyber-30@users.noreply.github.com>
2025-06-13 12:29:27 +0530

    Update main.go
    Insert line (7,8)

--- cmd/stealthdropper/main.go
+++ cmd/stealthdropper/main.go
@@ -3,7 +3,10 @@ package main
 import "fmt"
 import "os"
 import "github.com/Cyber-30/StealthDropper/encoder"
-import "github.com/Cyber-30/StealthDropper/exec/exec/exec_win"
+import "github.com/Cyber-30/StealthDropper/exec/exec/exec_win.go"
+import "github.com/Cyber-30/StealthDropper/exec/exec/exec_mac.go"
+import "github.com/Cyber-30/StealthDropper/exec/exec/exec_linux.go"
+
 
 func decode(data []byte) []byte {
 	key := byte(0xAA)
